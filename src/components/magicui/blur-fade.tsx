9b43cd892dc79a933f54f965644fa20ee61a99f2
vandriodd <ludasaadi@gmail.com>
2024-09-10 00:36:33 -0300

    Fix margin type

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -3,6 +3,8 @@
 import { AnimatePresence, motion, useInView, Variants } from "framer-motion";
 import { useRef } from "react";
 
+type MarginValue = `${number}${"px" | "%"}`;
+
 interface BlurFadeProps {
   children: React.ReactNode;
   className?: string;
@@ -14,7 +16,7 @@ interface BlurFadeProps {
   delay?: number;
   yOffset?: number;
   inView?: boolean;
-  inViewMargin?: string;
+  inViewMargin?: MarginValue;
   blur?: string;
 }
 const BlurFade = ({
